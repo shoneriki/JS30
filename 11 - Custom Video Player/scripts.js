b0085f8dd6486b97e4a097646cf06dff8459798c
shoneriki <ikirenohs@gmail.com>
2020-12-14 13:33:45 +0900

    11-start

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,20 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+
+
+// Build out functions
+
+
+
+// Hook up the event listeners
+
+
+
+
+const progressBar = document.querySelector(".progress__filled");
+
+document.addEventListener("click" => {
+
+})
