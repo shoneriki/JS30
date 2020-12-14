dab76c94b901d7585f76b1079f05563d048052f9
shoneriki <ikirenohs@gmail.com>
2020-12-15 08:51:47 +0900

    12-completed

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,4 @@
-// Get our elements
+// Get elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -8,7 +8,7 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
-// toggle will not work
+// nothing works
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
@@ -53,4 +53,4 @@ let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);'mousedown', () => mousedown = false);
+progress.addEventListener('mouseup', () => mousedown = false);
