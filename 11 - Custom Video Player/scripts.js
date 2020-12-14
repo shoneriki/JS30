5ff1d8ca35648f4244184a867c5abca5d9a879ff
shoneriki <ikirenohs@gmail.com>
2020-12-14 13:59:37 +0900

    11-togglePlay

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,27 +1,20 @@
 // Get our elements
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled')
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 
-
-
-
 // Build out functions
 
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
 
 // Hook up the event listeners
-
-
-
-
-const progressBar = document.querySelector(".progress__filled");
-
-document.addEventListener("click" => {
-
-})
